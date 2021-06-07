7ab71a7150214eb97011cd4839afc7baf0e3a953
lukaszdabrzalski <dab.lukasz@25wat.com>
2021-06-07 09:44:24 +0200

    remove branch name

--- includes/class-dashboard.php
+++ includes/class-dashboard.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( ! class_exists( 'version_st24_dashboard' ) ) {
+if (!class_exists('version_st24_dashboard')) {
 
     class version_st24_dashboard {
 
@@ -9,30 +9,30 @@ if ( ! class_exists( 'version_st24_dashboard' ) ) {
 
         public function __construct() {
             // register actions
-            add_action( 'admin_init', array( &$this, 'admin_init' ) );
-            add_action( 'admin_menu', array( &$this, 'add_menu' ) );
+            add_action('admin_init', array(&$this, 'admin_init'));
+            add_action('admin_menu', array(&$this, 'add_menu'));
 
-            $this->repository = new version_st24_repository( '../' );
+            $this->repository = new version_st24_repository('../');
         }
 
         public function admin_init() {
-            if ( isset( $_GET['sync'] ) ) {
-                if ( true === filter_var( $_GET['sync'], FILTER_VALIDATE_BOOLEAN ) ) {
+            if (isset($_GET['sync'])) {
+                if (true === filter_var($_GET['sync'], FILTER_VALIDATE_BOOLEAN)) {
 
-                    $version = @file_get_contents( './../VERSION' );
+                    $version = @file_get_contents('./../VERSION');
 
-                    if ( $version ) {
+                    if ($version) {
 
                         $current_user = wp_get_current_user();
 
-                        $user_email = strlen( $current_user->user_email ) ? $current_user->user_email : $current_user->display_login;
-                        $user_name  = strlen( $current_user->user_nicename ) ? $current_user->user_nicename : $current_user->display_name;
+                        $user_email = strlen($current_user->user_email) ? $current_user->user_email : $current_user->display_login;
+                        $user_name  = strlen($current_user->user_nicename) ? $current_user->user_nicename : $current_user->display_name;
 
-                        $output = $this->repository->syncRepository( $version, $this->message, $user_email, $user_name );
+                        $output = $this->repository->syncRepository($version, $this->message, $user_email, $user_name);
 
-                        set_transient( 'version_sync_action', 'started', 60 );
+                        set_transient('version_sync_action', 'started', 60);
 
-                        wp_redirect( 'tools.php?page=version_st24' );
+                        wp_redirect('tools.php?page=version_st24');
                     }
                 }
             }
@@ -46,7 +46,7 @@ if ( ! class_exists( 'version_st24_dashboard' ) ) {
                 'Version ST24',
                 'manage_options',
                 'version_st24',
-                array( &$this, 'plugin_dashboard_page' )
+                array(&$this, 'plugin_dashboard_page')
             );
         }
 
@@ -54,11 +54,9 @@ if ( ! class_exists( 'version_st24_dashboard' ) ) {
 
             $repo_config    = $this->repository->getConfig();
             $current_branch = $this->repository->getCurrentBranchName();
-            $branches       = $this->repository->getBranches();
             $commits_limit  = 20;
-            $commits        = $this->repository->getCommits( $commits_limit );
+            $commits        = $this->repository->getCommits($commits_limit);
             $status         = $this->repository->hasChanges();
-            $release        = $this->repository->getRelease( $repo_config, $branches );
             $disabled       = $status ? '' : 'disabled';
 
             echo '<div class="wrap version-dashboard">';
@@ -76,7 +74,6 @@ if ( ! class_exists( 'version_st24_dashboard' ) ) {
                 'Date',
                 'Author',
                 'Commit',
-                'Branch',
                 'Details',
             );
 
@@ -85,7 +82,7 @@ if ( ! class_exists( 'version_st24_dashboard' ) ) {
 
             echo '<thead>';
             echo '<tr>';
-            foreach ( $table_col as $key => $col ) {
+            foreach ($table_col as $key => $col) {
                 echo '<th>' . $col . '</th>';
             }
             echo '</tr>';
@@ -93,21 +90,7 @@ if ( ! class_exists( 'version_st24_dashboard' ) ) {
 
             echo '<tbody>';
 
-            $last_branch = '---';
-            foreach ( $commits as $key => $commit ) {
-
-                $branch = str_replace( 'branchname', $current_branch, $commit['branch'] );
-
-                if ( false === strpos( $branch, 'HEAD' ) ) {
-                    $branch = substr( $branch, strpos( $branch, ',' ) );
-                    $branch = str_replace( array( '(', ')', ', ' ), '', $branch );
-                } else {
-                    $branch = str_replace( array( '(', ')' ), '', $branch );
-                }
-
-                if ( false !== strpos( $commit['name'], $this->message ) ) {
-                    $branch = $release;
-                }
+            foreach ($commits as $key => $commit) {
 
                 echo '<tr>';
 
@@ -115,8 +98,7 @@ if ( ! class_exists( 'version_st24_dashboard' ) ) {
                 echo '<td class="center">' . $commit['hash'] . '</td>';
                 echo '<td class="date">' . $commit['date'] . '</td>';
                 echo '<td class="author">' . $commit['author'] . '</td>';
-                echo '<td class="commit">' . htmlentities( $commit['name'] ) . '</td>';
-                echo '<td class="branch">' . $branch . '</td>';
+                echo '<td class="commit">' . htmlentities($commit['name']) . '</td>';
                 echo '<td class="details">' . $commit['files'] . '</td>';
 
                 echo '<tr>';
@@ -130,14 +112,14 @@ if ( ! class_exists( 'version_st24_dashboard' ) ) {
 
             echo '<div>';
             echo '<h5>DEBUG log - last 20 lines</h5>';
-            echo '<textarea id="debug_log" class="debug_log" placeholder="unknow">' . $this->tailCustom( ABSPATH . '/wp-content/debug.log', 20 ) . '</textarea>';
+            echo '<textarea id="debug_log" class="debug_log" placeholder="unknow">' . $this->tailCustom(ABSPATH . '/wp-content/debug.log', 20) . '</textarea>';
             echo '<button id="debug_button">take_focus</button>';
             echo '</div>';
 
             echo '<h5>Repository config</h5>';
             echo '<div class="repo-config">';
 
-            foreach ( $repo_config as $param ) {
+            foreach ($repo_config as $param) {
                 echo '<div>' . $param . '</div>';
             }
             echo '</div>';
@@ -162,45 +144,41 @@ if ( ! class_exists( 'version_st24_dashboard' ) ) {
          * @link http://stackoverflow.com/a/15025877/995958
          * @license http://creativecommons.org/licenses/by/3.0/
          */
-        private function tailCustom( $filepath, $lines = 1, $adaptive = true ) {
-            $f = @fopen( $filepath, 'rb' );
-            if ( $f === false ) {
+        private function tailCustom($filepath, $lines = 1, $adaptive = true) {
+            $f = @fopen($filepath, 'rb');
+            if ($f === false) {
                 return false;
             }
 
-            if ( ! $adaptive ) {
+            if (!$adaptive) {
                 $buffer = 4096;
             } else {
-                $buffer = ( $lines < 2 ? 64 : ( $lines < 10 ? 512 : 4096 ) );
+                $buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
             }
 
-            fseek( $f, -1, SEEK_END );
+            fseek($f, -1, SEEK_END);
 
-            if ( fread( $f, 1 ) !== "\n" ) {
+            if (fread($f, 1) !== "\n") {
                 $lines--;
             }
 
             $output = '';
             $chunk  = '';
-            while ( ftell( $f ) > 0 && $lines >= 0 ) {
-                $seek = min( ftell( $f ), $buffer );
-                fseek( $f, -$seek, SEEK_CUR );
-                $output = ( $chunk = fread( $f, $seek ) ) . $output;
-                fseek( $f, -mb_strlen( $chunk, '8bit' ), SEEK_CUR );
-                $lines -= substr_count( $chunk, "\n" );
-
+            while (ftell($f) > 0 && $lines >= 0) {
+                $seek = min(ftell($f), $buffer);
+                fseek($f, -$seek, SEEK_CUR);
+                $output = ($chunk = fread($f, $seek)) . $output;
+                fseek($f, -mb_strlen($chunk, '8bit'), SEEK_CUR);
+                $lines -= substr_count($chunk, "\n");
             }
 
-            while ( $lines++ < 0 ) {
-                $output = substr( $output, strpos( $output, "\n" ) + 1 );
-
+            while ($lines++ < 0) {
+                $output = substr($output, strpos($output, "\n") + 1);
             }
 
-            fclose( $f );
+            fclose($f);
 
-            return trim( $output );
+            return trim($output);
         }
-
     }
-
 }
